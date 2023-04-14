7ae9e817661987c81b57b9b7bb0930eadcf52a06
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-04-14 14:20:44 +0300

    ...

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,47 +1,56 @@
 ﻿<template>
-  <base-card>
-    <div class="card-header">
-      <span class="title">Dnipro, UA</span>
-      <a class="btn btn-icon" href="#">
-        <CheckmarkOutline />
-      </a>
-      <a class="btn btn-icon" href="#">
-        <CheckmarkOutline />
-      </a>
-      <a class="btn btn-icon" href="#">
-        <CheckmarkOutline />
-      </a>
-    </div>
-    <nav class="card-nav">
-      <selector :options="cardTabs" v-model="selectedTab" />
-    </nav>
-    <component :is="selectedTabView" />
-  </base-card>
+  <div class="card">
+    <slot>
+      <div class="card-header">
+        <span class="title">Dnipro, UA</span>
+        <a
+          href="#" @click="toggleFavorites"
+          class="btn btn-icon"
+          :class="{'has-accent': isInFavorites}"
+        >
+          <IconFavs />
+        </a>
+        <a class="btn btn-icon" href="#">
+          <IconReload />
+        </a>
+        <a class="btn btn-icon" href="#">
+          <IconDelete />
+        </a>
+      </div>
+      <nav class="card-nav">
+        <selector :options="cardTabs" v-model="selectedTab" />
+      </nav>
+      <component :is="selectedTabView" />
+    </slot>
+  </div>
 
-  <base-card>
-    <div class="card-header">
-      <span class="title">Dnipro, UA</span>
-      <a class="btn btn-icon" href="#">
-        <CheckmarkOutline />
-      </a>
-      <a class="btn btn-icon" href="#">
-        <CheckmarkOutline />
-      </a>
-      <a class="btn btn-icon" href="#">
-        <CheckmarkOutline />
-      </a>
-    </div>
-    <nav class="card-nav">
-      <selector :options="cardTabs" v-model="selectedTab" />
-    </nav>
-    <component :is="infoWeek" />
-  </base-card>
+  <div class="card">
+    <slot>
+      <div class="card-header">
+        <span class="title">Dnipro, UA</span>
+        <a class="btn btn-icon" href="#">
+          <IconFavs />
+        </a>
+        <a class="btn btn-icon" href="#">
+          <IconReload />
+        </a>
+        <a class="btn btn-icon" href="#">
+          <IconDelete />
+        </a>
+      </div>
+      <nav class="card-nav">
+        <selector :options="cardTabs" v-model="selectedTab" />
+      </nav>
+      <component :is="infoWeek" />
+    </slot>
+  </div>
 </template>
 
 <script setup>
-  import { ref, computed, resolveComponent } from 'vue'
-  import baseCard from '@/components/base-card'
-  import CheckmarkOutline from '@/assets/icons/checkmark-outline.svg'
+  import { ref, computed } from 'vue'
+  import IconFavs from '@/assets/icons/star.svg'
+  import IconReload from '@/assets/icons/reload.svg'
+  import IconDelete from '@/assets/icons/delete.svg'
   import selector from '@/components/selector'
   import infoDay from '@/components/info-day'
   import infoDaylyGraph from '@/components/info-dayly-graph'
@@ -57,25 +66,38 @@
     infoWeek
   }
 
+  const isInFavorites = ref(false)
   const selectedTab = ref(cardTabs[0])
-  const selectedTabView = computed(() => components[selectedTab.value.value])
+  
+  const selectedTabView = computed(() => 
+    components[selectedTab.value.value]
+  )
+  
+  const classes = computed(() => ({
+    'has-accent': isInFavorites.value
+  }))
+
+
+  function toggleFavorites() {
+    isInFavorites.value = !isInFavorites.value
+  }
 </script>
 
 <style lang="scss" scoped>
 .card {
-  // background: #088;
   font-size: 1rem;
   color: $text-100;
+  background: $bg-700;
   padding: .5rem;
+  cursor: default;
 
   &-header {
     font-size: 1.5rem;
-    // background: $bg-600;
     height: 2rem;
     line-height: 2rem;
     display: grid;
     grid-template-columns: 1fr repeat(3, 32px);
-    // padding: 0 .5rem;
+    grid-gap: .25rem;
   }
 
   &-nav {
