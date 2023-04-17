ed9580a2650793bf9b14e6b8362324b47ff41050
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2023-04-17 10:07:35 +0300

    added 'card-container' component

---
+++ src/components/tab-container.vue
@@ -0,0 +1,22 @@
+﻿<template>
+	<div class="tab-container">
+		<slot></slot>
+		<spinner />
+	</div>
+</template>
+
+<script setup>
+	// import { ref, reactive } from 'vue'
+	import spinner from '@/components/modal/spinner'
+
+	const props = defineProps({
+	})
+</script>
+
+<style lang="scss" scoped>
+.tab-container {
+	position: relative;
+	margin-top: .25rem;
+}
+</style>
+	
